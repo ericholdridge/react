f132b44f023894d2dee8b84f3da5ece87ff94582
ericholdridge <eholdridge2@gmail.com>
2024-03-02 15:55:55 -0600

    Working app with Footer component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from "./components/Footer"
+
+function App() {
+    return (
+        <section>
+            <Footer></Footer>
+        </section>
+    )
+}
+
+export default App;
